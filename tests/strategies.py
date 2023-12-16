82c428e59c73ea8e116d08cd24f9bff3a56dbdaa
Marat Subkhankulov <marat@tomato.ai>
2023-12-16 01:07:18 -0800

    Task0.2

--- tests/strategies.py
+++ tests/strategies.py
@@ -9,7 +9,9 @@ settings.load_profile("ci")
 
 small_ints = integers(min_value=1, max_value=3)
 small_floats = floats(min_value=-100, max_value=100, allow_nan=False)
+positive_small_floats = floats(min_value=1e2, max_value=100, allow_nan=False)
 med_ints = integers(min_value=1, max_value=20)
+minus_one_to_one = floats(min_value=-1, max_value=1, allow_nan=False)
 
 
 def assert_close(a: float, b: float) -> None:
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -6,12 +6,14 @@ from hypothesis.strategies import lists
 
 from minitorch import MathTest
 from minitorch.operators import (
+    EPS,
     add,
     addLists,
     eq,
     id,
     inv,
     inv_back,
+    log,
     log_back,
     lt,
     max,
@@ -25,7 +27,7 @@ from minitorch.operators import (
     sum,
 )
 
-from .strategies import assert_close, small_floats
+from .strategies import assert_close, small_floats, minus_one_to_one, positive_small_floats
 
 # ## Task 0.1 Basic hypothesis tests.
 
@@ -99,7 +101,7 @@ def test_eq(a: float) -> None:
 
 
 @pytest.mark.task0_2
-@given(small_floats)
+@given(minus_one_to_one)
 def test_sigmoid(a: float) -> None:
     """Check properties of the sigmoid function, specifically
     * It is always between 0.0 and 1.0.
@@ -107,45 +109,47 @@ def test_sigmoid(a: float) -> None:
     * It crosses 0 at 0.5
     * It is  strictly increasing.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert 0.0 <= sigmoid(a) <= 1.0
+    assert_close(1.0 - sigmoid(a), sigmoid(-a))
+    assert sigmoid(0.0) == 0.5
+    assert sigmoid(a) < sigmoid(a + EPS)
 
 
 @pytest.mark.task0_2
-@given(small_floats, small_floats, small_floats)
-def test_transitive(a: float, b: float, c: float) -> None:
+@given(small_floats, positive_small_floats, positive_small_floats)
+def test_transitive(a: float, offset1: float, offset2: float) -> None:
     "Test the transitive property of less-than (a < b and b < c implies a < c)"
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
-
+    b = a + offset1
+    c = b + offset2
+    lt(a, b) == 1.0 and lt(b, c) == 1.0 and lt(a, c) == 1.0
 
 @pytest.mark.task0_2
-def test_symmetric() -> None:
+@given(small_floats, small_floats)
+def test_symmetric(a: float, b: float) -> None:
     """
     Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert mul(a, b) == mul(b, a)
 
 
 @pytest.mark.task0_2
-def test_distribute() -> None:
+@given(small_floats, small_floats, small_floats)
+def test_distribute(a, b, c) -> None:
     r"""
     Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(a, add(b, c)), add(mul(a, b), mul(a, c)))
 
 
 @pytest.mark.task0_2
-def test_other() -> None:
+@given(positive_small_floats)
+def test_log_exponent(x) -> None:
     """
-    Write a test that ensures some other property holds for your functions.
+    Test that natural log inverts
     """
-    # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(x, log_back(log(x), 1.0))
 
 
 # ## Task 0.3  - Higher-order functions
